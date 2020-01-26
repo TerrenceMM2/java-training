284ea69738cad2de3c0fb1a4dc5a75fe6d5a24ed
TerrenceMM2 <terrencemm2@gmail.com>
2020-01-26 11:27:42 -0600

    adds project 4-12

--- chapter4/projects/4-12/Board.java
+++ chapter4/projects/4-12/Board.java
@@ -4,22 +4,31 @@ import java.awt.*;
 public class Board {
 
     public static void main(String[] args) {
+        
+
         JFrame theGUI = new JFrame();
         theGUI.setSize(1000, 1000);
         theGUI.setTitle("Checkerboard");
 
+        String inputStr = JOptionPane.showInputDialog("Number of rows", "5");
+        if (inputStr == null) return;
+        int rows = Integer.parseInt(inputStr);
+        inputStr = JOptionPane.showInputDialog("Number of columns", "5");
+        if (inputStr == null) return;
+        int cols = Integer.parseInt(inputStr);
+
         theGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container pane = theGUI.getContentPane();
-        pane.setLayout(new GridLayout(8, 8));
+        pane.setLayout(new GridLayout(rows, cols));
 
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
-                if ((row % 2) == (col % 2)) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if ((i % 2) == (j % 2)) {
                     ColorPanel panel = new ColorPanel(Color.black);
                     pane.add(panel);
                 } else {
                     ColorPanel panel = new ColorPanel(Color.red);
-                pane.add(panel);
+                 pane.add(panel);
                 }
             }
         }
