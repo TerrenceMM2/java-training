4f8320aca65a77a4a5b27dacad8e5bc82195836f
TerrenceMM2 <terrencemm2@gmail.com>
2020-01-27 14:21:17 -0600

    adds project 5-11

---
+++ chapter5/projects/5-11/Sound.java
@@ -0,0 +1,26 @@
+import sounds.APSoundClip;
+import sounds.Sample;
+import java.util.Scanner;
+
+public class Sound {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a volume factor: ");
+        double factor = reader.nextDouble();
+        APSoundClip clip = new APSoundClip("money.wav");
+        for (Sample s : clip) {
+            int newValue = (int)(s.getValue() * factor);
+            if (newValue > s.MAX_VALUE) {
+                newValue = s.MAX_VALUE;
+                s.setValue(newValue);
+            } else if (newValue < s.MIN_VALUE) {
+                newValue = s.MIN_VALUE;
+                s.setValue(newValue);
+            }
+        }
+        clip.draw();
+        clip.play();
+    }
+
+}
