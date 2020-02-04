3e2776324125d9f569b7e3154d6c93826d2e6659
TerrenceMM2 <terrencemm2@gmail.com>
2020-02-04 14:59:47 -0600

    adds completed 6-5 project

--- chapter6/projects/6-5/src/org/tm/BankAccount.java
+++ chapter6/projects/6-5/src/org/tm/BankAccount.java
@@ -2,4 +2,44 @@ package org.tm;
 
 public class BankAccount {
 
+	private String name = "";
+	private int balance = 0;
+	
+	public BankAccount(String newName, int newBalance) {
+		name = newName;
+		balance = newBalance;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public void setBalance(int newBalance) {
+		balance = newBalance;
+	}
+	
+	public int deposit(int amount) {
+		balance += amount;
+		return balance;
+	}
+	
+	public int withdrawl(int amount) {
+		balance -= amount;
+		return balance;
+	}
+	
+	public String toString() {
+		String str;
+		str = name + ", you have $" + balance + " in your account.";
+		return str;
+	}
+	
 }
--- chapter6/projects/6-5/src/org/tm/Tester.java
+++ chapter6/projects/6-5/src/org/tm/Tester.java
@@ -3,8 +3,24 @@ package org.tm;
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is a test.");
+
+		BankAccount terrence = new BankAccount("Terrence", 1000);
+		
+		terrence.deposit(100);
+		
+		System.out.println(terrence);
+		
+		terrence.withdrawl(250);
+		
+		System.out.println(terrence);
+		
+		terrence.setName("Terrence Mahnken");
+		
+		System.out.println(terrence);
+		
+		terrence.setBalance(2000);
+		
+		System.out.println(terrence);
 	}
 
 }
