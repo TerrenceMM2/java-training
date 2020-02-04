cc4219de0f6f13f847512413a83e0f67fee128aa
TerrenceMM2 <terrencemm2@gmail.com>
2020-02-04 14:48:25 -0600

    adds completed 6-4 project

--- chapter6/projects/6-4/src/org/tm/Dice.java
+++ chapter6/projects/6-4/src/org/tm/Dice.java
@@ -2,4 +2,24 @@ package org.tm;
 
 public class Dice {
 	
+	private int show;
+	
+	public Dice() {
+		show = 0;
+	}
+	
+	public int getNum() {
+		return show;
+	}
+	
+	public int roll() {
+		show = (int)(Math.random() * 6 + 1);
+		return show;
+	}
+	
+	public String toString() {
+		String str;
+		str = "You rolled a " + show;
+		return str;
+	}
 }
--- chapter6/projects/6-4/src/org/tm/Tester.java
+++ chapter6/projects/6-4/src/org/tm/Tester.java
@@ -1,10 +1,41 @@
 package org.tm;
+import java.util.Scanner;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is a test 2");
+		
+		Scanner reader = new Scanner(System.in);
+
+		Dice die1 = new Dice();
+		Dice die2 = new Dice();
+		int dollars;
+		int maxDollars = 0;
+		int count = 0;
+		int countAtMax = 0;
+		
+		System.out.print("How many dollars do you have? ");
+        dollars = reader.nextInt();
+		
+        while (dollars > 0) {
+        	count++;
+        	int die1Value = die1.roll();
+        	int die2Value = die2.roll();
+        	
+        	if (die1Value + die2Value == 7) 
+        		dollars += 4;
+        	else
+        		dollars -= 1;
+        	
+        	if (dollars > maxDollars) {
+        		maxDollars = dollars;
+        		countAtMax = count;
+        	}
+        }
+        
+        System.out.println("You are broke after " + count + " rolls.");
+        System.out.println("You should have quite after " + countAtMax + " rolls when you had $" + maxDollars + ".");
+        
 	}
 
 }
